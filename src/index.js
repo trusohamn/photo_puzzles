9e889ec2d792b2bafa9fe18382537f3017913dde
trusohamn <marta.trusohamn@gmail.com>
2019-06-10 23:20:47 +0200

    styling

--- src/index.js
+++ src/index.js
@@ -2,22 +2,22 @@ import './style.scss';
 
 document.querySelector('.container').addEventListener('click', (e) => {
     if (e.target.tagName === 'BUTTON') {
-        const style = getComputedStyle(e.target.parentNode);
+        const style = getComputedStyle(e.target.parentNode.parentNode);
         switch (e.target.value) {
             case 'down': {
-                e.target.parentNode.style['grid-row-start'] = Number(style['grid-row-start']) + 1;
+                e.target.parentNode.parentNode.style['grid-row-start'] = Number(style['grid-row-start']) + 1;
                 break;
             }
             case 'up': {
-                e.target.parentNode.style['grid-row-start'] = Number(style['grid-row-start']) - 1;
+                e.target.parentNode.parentNode.style['grid-row-start'] = Number(style['grid-row-start']) - 1;
                 break;
             }
             case 'right': {
-                e.target.parentNode.style['grid-column-start'] = Number(style['grid-column-start']) + 1;
+                e.target.parentNode.parentNode.style['grid-column-start'] = Number(style['grid-column-start']) + 1;
                 break;
             }
             case 'left': {
-                e.target.parentNode.style['grid-column-start'] = Number(style['grid-column-start']) - 1;
+                e.target.parentNode.parentNode.style['grid-column-start'] = Number(style['grid-column-start']) - 1;
                 break;
             }
         }

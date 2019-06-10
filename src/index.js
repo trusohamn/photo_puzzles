02578ec580a3beb49d1ccfef58dbf776e754daa6
trusohamn <marta.trusohamn@gmail.com>
2019-06-10 22:21:24 +0200

    moving puzzles

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
 import './style.scss';
 
-const name = 'Martaa'
-console.log("hello,world", name);   
+document.querySelector('.container').addEventListener('click', (e) => {
+    if (e.target.tagName === 'BUTTON') {
+        const style = getComputedStyle(e.target.parentNode);
+        switch (e.target.value) {
+            case 'down': {
+                e.target.parentNode.style['grid-row-start'] = Number(style['grid-row-start']) + 1;
+                break;
+            }
+            case 'up': {
+                e.target.parentNode.style['grid-row-start'] = Number(style['grid-row-start']) - 1;
+                break;
+            }
+            case 'right': {
+                e.target.parentNode.style['grid-column-start'] = Number(style['grid-column-start']) + 1;
+                break;
+            }
+            case 'left': {
+                e.target.parentNode.style['grid-column-start'] = Number(style['grid-column-start']) - 1;
+                break;
+            }
+        }
+
+        //e.target.parentNode.style['grid-area'] = newarea;
+    }
+});

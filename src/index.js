6b58e526c66c6e38f6f44e1a73da498e6f05a79e
trusohamn <marta.trusohamn@gmail.com>
2019-06-11 14:42:26 +0200

    animating puzzle transition

--- src/index.js
+++ src/index.js
@@ -2,25 +2,47 @@ import './style.scss';
 
 document.querySelector('.container').addEventListener('click', (e) => {
     if (e.target.tagName === 'BUTTON') {
-        const style = getComputedStyle(e.target.parentNode.parentNode);
+        // https://css-tricks.com/animating-layouts-with-the-flip-technique/
+        const elm = e.target.parentNode.parentNode;
+        const style = getComputedStyle(elm);
+        const first = elm.getBoundingClientRect();
+
         switch (e.target.value) {
             case 'down': {
-                e.target.parentNode.parentNode.style['grid-row-start'] = Number(style['grid-row-start']) + 1;
+                elm.style['grid-row-start'] = Number(style['grid-row-start']) + 1;
                 break;
             }
             case 'up': {
-                e.target.parentNode.parentNode.style['grid-row-start'] = Number(style['grid-row-start']) - 1;
+                elm.style['grid-row-start'] = Number(style['grid-row-start']) - 1;
                 break;
             }
             case 'right': {
-                e.target.parentNode.parentNode.style['grid-column-start'] = Number(style['grid-column-start']) + 1;
+                elm.style['grid-column-start'] = Number(style['grid-column-start']) + 1;
                 break;
             }
             case 'left': {
-                e.target.parentNode.parentNode.style['grid-column-start'] = Number(style['grid-column-start']) - 1;
+                elm.style['grid-column-start'] = Number(style['grid-column-start']) - 1;
                 break;
             }
+            // Last, Invert, Play: the flip() method does it all
         }
+        const last = elm.getBoundingClientRect();
+        const deltaX = first.left - last.left;
+        const deltaY = first.top - last.top;
+
+        elm.animate([{
+            transformOrigin: 'top left',
+            transform: `
+              translate(${deltaX}px, ${deltaY}px)
+            `
+          }, {
+            transformOrigin: 'top left',
+            transform: 'none'
+          }], {
+            duration: 500,
+            easing: 'ease-in-out',
+            fill: 'both'
+          });
     } else {
         randomImages();
     }

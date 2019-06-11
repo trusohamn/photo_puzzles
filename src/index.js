a945f20876fc56473159f025bb7407d3c5f5d902
trusohamn <marta.trusohamn@gmail.com>
2019-06-11 12:57:01 +0200

    randomize

--- src/index.js
+++ src/index.js
@@ -25,3 +25,27 @@ document.querySelector('.container').addEventListener('click', (e) => {
         //e.target.parentNode.style['grid-area'] = newarea;
     }
 });
+
+function randomImages() {
+    const divs = Array.from(document.querySelectorAll('.container>div'));
+    console.log(shuffle(divs));
+    let count = 0
+    for (let r = 1; r <= 3; r++) {
+        for (let c = 1; c <= 3; c++) {
+            divs[count].style['grid-row-start'] = r;
+            divs[count].style['grid-column-start'] = c;
+            count++;
+        }
+    }
+}
+
+
+function shuffle(a) {
+    for (let i = 0; i < a.length; i++) {
+        const j = Math.floor(Math.random() * a.length); //random index j
+        [a[i], a[j]] = [a[j], a[i]]; //switch
+    }
+    return a;
+}
+
+window.onload = randomImages();
